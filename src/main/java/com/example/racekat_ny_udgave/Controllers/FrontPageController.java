9eb2601a65d8fa455c197cb6fea6f08c659d622e
Mikkelsen1337 <marcusmarchholdt@gmail.com>
2025-04-06 01:52:41 +0200

    CSS og HTML

--- src/main/java/com/example/racekat_ny_udgave/Controllers/FrontPageController.java
+++ src/main/java/com/example/racekat_ny_udgave/Controllers/FrontPageController.java
@@ -1,6 +1,16 @@
 package com.example.racekat_ny_udgave.Controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
 public class FrontPageController {
+    @GetMapping("/")
+    public String frontPage() {
+        return "index";
+    }
 
 
 
